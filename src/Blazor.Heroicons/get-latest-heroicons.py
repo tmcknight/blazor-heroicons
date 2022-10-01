ea822493caa3558dfe3bf113f28d5b2564310063
tmcknight <tom@tmcknight.dev>
2022-10-01 17:05:16 -0400

    Heroicon components

---
+++ src/Blazor.Heroicons/get-latest-heroicons.py
@@ -0,0 +1,98 @@
+from glob import iglob
+import os
+import urllib.request
+import json
+from pathlib import Path
+import cgi
+import shutil
+import tarfile
+from venv import create
+
+
+def main():
+    # clean up prior runs
+    if Path("tmp").exists():
+        print("Cleaning up existing tmp files...")
+        shutil.rmtree("tmp")
+
+    # create required directories
+    Path("Solid").mkdir(exist_ok=True)
+    Path("Outline").mkdir(exist_ok=True)
+    Path("Mini").mkdir(exist_ok=True)
+    Path("tmp").mkdir(exist_ok=True)
+
+    # get latest release info
+    print("Getting latest release info...")
+    with urllib.request.urlopen("https://api.github.com/repos/tailwindlabs/heroicons/releases/latest") as url:
+        data = json.load(url)
+        version = data["tag_name"]
+        download_url = data["tarball_url"]
+
+    # download tarball
+    print(f"Downloading {version}...")
+    with urllib.request.urlopen(download_url) as remote_file:
+        value, params = cgi.parse_header(
+            remote_file.info()['Content-Disposition'])
+        tar_filename = params["filename"]
+
+    tar_filename = f"tmp/{tar_filename}"
+    urllib.request.urlretrieve(download_url, tar_filename)
+    print(f"Downloaded latest release to {tar_filename}")
+
+    # extract tar
+
+    print("Extracting tar file...")
+    tar = tarfile.open(tar_filename)
+    tar.extractall(members=optimized_files(
+        tar, os.path.commonprefix(tar.getnames())), path="tmp")
+    tar.close()
+    os.remove(tar_filename)
+
+    # loop through files
+    print("Looping through svg files and creating razor components...")
+    create_blazor_component_files("Mini", "tmp/**/20/solid/*.svg")
+    create_blazor_component_files("Solid", "tmp/**/24/solid/*.svg")
+    create_blazor_component_files("Outline", "tmp/**/24/outline/*.svg")
+
+    # clean up
+    print("Cleaning up tmp files...")
+    shutil.rmtree("tmp")
+
+
+def create_blazor_component_files(icon_type, glob):
+    print(f"Creating {icon_type} razor components...")
+    file_list = [f for f in iglob(glob, recursive=True) if os.path.isfile(f)]
+    count = 0
+    for file in file_list:
+        content = ""
+        # convert svg content to blazor component
+        with open(file) as svg:
+            content = "@inherits HeroiconBase\n"
+            content = content + svg.read()
+            content = content.replace("aria-hidden=\"true\">",
+                                      "aria-hidden=\"true\" @attributes=\"AdditionalAttributes\">")
+
+        # write file
+        with open(f"{icon_type}/{to_title_case(Path(file).stem)}.razor", 'w') as blazor_component:
+            blazor_component.write(content)
+
+        count = count + 1
+    print(f"Created {count} {icon_type} razor components")
+
+
+def optimized_files(members, root):
+    for tarinfo in members:
+        if tarinfo.name.startswith(f"{root}/optimized"):
+            yield tarinfo
+
+
+def to_title_case(text):
+    s = text.replace("-", " ").replace("_", " ")
+    s = s.split()
+    if len(text) == 0:
+        return text
+    return s[0].capitalize() + ''.join(i.capitalize() for i in s[1:])
+
+
+if __name__ == "__main__":
+    main()
