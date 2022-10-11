7df9f24c3c6b277712a281964eec8719ac406e2e
Thomas McKnight <tmcknight@bluewaterpower.com>
2022-10-11 09:30:26 -0400

    Add xml code comments

--- src/Blazor.Heroicons/HeroiconBase.cs
+++ src/Blazor.Heroicons/HeroiconBase.cs
@@ -4,13 +4,28 @@ namespace Blazor.Heroicons;
 
 public abstract class HeroiconBase : ComponentBase
 {
+    /// <summary>
+    /// Gets or sets a collection of additional attributes that will be applied to the created element.
+    /// </summary>
     [Parameter(CaptureUnmatchedValues = true)]
     public Dictionary<string, object> AdditionalAttributes { get; set; } = new();
 }
 
+/// <summary>
+/// The type/size of the icon.
+/// </summary>
 public enum HeroiconType
 {
+    /// <summary>
+    /// For primary navigation and marketing sections, with a filled appearance.
+    /// </summary>
     Solid,
+    /// <summary>
+    /// For primary navigation and marketing sections, with an outlined appearance.
+    /// </summary>
     Outline,
+    /// <summary>
+    /// For smaller elements like buttons, form elements, and to support text.
+    /// </summary>
     Mini
 }
