d0b64fe8ae19dba3e5d433b5d5ac07fbc890db91
Thomas McKnight <tom@tmcknight.dev>
2026-02-27 22:38:05 -0500

    Add tests for RandomHeroicon seed functionality

--- tests/Blazor.Heroicons.Tests/RandomHeroiconTests.cs
+++ tests/Blazor.Heroicons.Tests/RandomHeroiconTests.cs
@@ -32,7 +32,7 @@ public class RandomHeroiconTests : BunitContext
     [TestMethod]
     public void ChangingUnmatchedAttributeRetainsIcon()
     {
-        // Arrange 
+        // Arrange
         var cut = Render<RandomHeroicon>(parameters => parameters
             .Add(p => p.Type, HeroiconType.Solid));
         var originalType = cut.Instance.Type;
@@ -45,5 +45,73 @@ public class RandomHeroiconTests : BunitContext
         Assert.AreEqual("h-10 w-10", cut.Find("svg").GetAttribute("class"));
         Assert.AreEqual(HeroiconType.Solid, cut.Instance.Type);
         Assert.AreEqual(originalType, cut.Instance.Type);
+    }
+
+    [TestMethod]
+    public void SameSeedProducesSameIcon()
+    {
+        // Arrange & Act
+        var cut1 = Render<RandomHeroicon>(parameters => parameters
+            .Add(p => p.Seed, 42));
+        var cut2 = Render<RandomHeroicon>(parameters => parameters
+            .Add(p => p.Seed, 42));
+
+        // Assert
+        Assert.AreEqual(cut1.Markup, cut2.Markup);
+    }
+
+    [TestMethod]
+    public void DifferentSeedsProduceDifferentIcons()
+    {
+        // Arrange & Act - use seeds known to produce different Random.Next results
+        var cut1 = Render<RandomHeroicon>(parameters => parameters
+            .Add(p => p.Seed, 1));
+        var cut2 = Render<RandomHeroicon>(parameters => parameters
+            .Add(p => p.Seed, 2));
+
+        // Assert
+        Assert.AreNotEqual(cut1.Markup, cut2.Markup);
+    }
+
+    [TestMethod]
+    public void ChangingSeedPicksNewIcon()
+    {
+        // Arrange
+        var cut = Render<RandomHeroicon>(parameters => parameters
+            .Add(p => p.Seed, 1));
+        var originalMarkup = cut.Markup;
+
+        // Act
+        cut.Render(parameters => parameters
+            .Add(p => p.Seed, 2));
+
+        // Assert
+        Assert.AreNotEqual(originalMarkup, cut.Markup);
+    }
+
+    [TestMethod]
+    public void SameSeedRetainsIconOnRerender()
+    {
+        // Arrange
+        var cut = Render<RandomHeroicon>(parameters => parameters
+            .Add(p => p.Seed, 42));
+        var originalMarkup = cut.Markup;
+
+        // Act - re-render with same seed
+        cut.Render(parameters => parameters
+            .Add(p => p.Seed, 42));
+
+        // Assert
+        Assert.AreEqual(originalMarkup, cut.Markup);
+    }
+
+    [TestMethod]
+    public void NullSeedIsDefault()
+    {
+        // Arrange & Act
+        var cut = Render<RandomHeroicon>();
+
+        // Assert
+        Assert.IsNull(cut.Instance.Seed);
     }
 }
